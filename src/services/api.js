61ee34841b29663892431b3ecbb0fcff5b262297
Abdulakeem abdulafeez Kolawole <kolawolecyber@users.noreply.github.com>
2025-08-22 16:46:22 +0100

    initial commit with deployed project

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // update if needed
+  baseURL: import.meta.env.VITE_API_BASE_URL, // update if needed
 });
 
 // Request interceptor: add token if available
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api"; // change to your backend port
+const API_BASE = import.meta.env.VITE_API_BASE_URL; // change to your backend port
 
 const signup = async (userData) => {
     try {
