b7207a26ebc12c7f4aba16eec7b465f4496f856d
Abdulakeem abdulafeez Kolawole <kolawolecyber@users.noreply.github.com>
2025-11-30 13:04:42 +0100

    Initial commit of new project

--- src/components/TransactionChart.jsx
+++ src/components/TransactionChart.jsx
@@ -21,8 +21,13 @@ const TransactionChart = ({ transactions }) => {
     const grouped = {};
 
     transactions.forEach((tx) => {
-      const date = new Date(tx.date);
-      const month = date.toLocaleString("default", {
+      const dateValue = tx.date || tx.createdAt;
+
+      const parsedDate = new Date(dateValue);
+
+      if (isNaN(parsedDate)) return; // skip bad dates
+
+      const month = parsedDate.toLocaleString("default", {
         month: "short",
         year: "numeric",
       });
@@ -31,12 +36,13 @@ const TransactionChart = ({ transactions }) => {
         grouped[month] = { month, income: 0, expense: 0 };
       }
 
-      if (tx.type === "income") grouped[month].income += tx.amount;
-      else grouped[month].expense += tx.amount;
+      const amt = Number(tx.amount);
+
+      if (tx.type === "income") grouped[month].income += amt;
+      else grouped[month].expense += amt;
     });
 
-    const finalData = Object.values(grouped);
-    setChartData(finalData);
+    setChartData(Object.values(grouped));
   }, [transactions]);
 
   return (
